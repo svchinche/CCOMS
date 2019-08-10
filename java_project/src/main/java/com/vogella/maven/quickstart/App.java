ec4aecf6ef21f5611838c1bd57b966c9643fe84f
suyog chinche <suyog.v.chinche@oracle.com>
2019-08-10 06:52:48 -0700

    add java project

---
+++ java_project/src/main/java/com/vogella/maven/quickstart/App.java
@@ -0,0 +1,13 @@
+package com.vogella.maven.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
