381b5e69fdfc3e841e7a45a6a10e44753e7bf0f5
amit parate <amit.parate@gmail.com>
2019-09-13 15:58:40 +0530

    merging Feature branch into develop

--- java_project/src/test/java/com/vogella/maven/quickstart/TestEmployeeDetails.java
+++ java_project/src/test/java/com/vogella/maven/quickstart/TestEmployeeDetails.java
@@ -1,31 +1,38 @@
 package com.vogella.maven.quickstart;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import org.testng.*;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 
 public class TestEmployeeDetails {
-	 EmpBusinessLogic empBusinessLogic = new EmpBusinessLogic();
-	   EmployeeDetails employee = new EmployeeDetails();
+    EmpBusinessLogic empBusinessLogic = new EmpBusinessLogic();
+    EmployeeDetails employee = new EmployeeDetails();
 
-	   //test to check appraisal
-	   @Test
-	   public void testCalculateAppriasal() {
-	      employee.setName("Rajeev");
-	      employee.setAge(25);
-	      employee.setMonthlySalary(8000);
-			
-	      double appraisal = empBusinessLogic.calculateAppraisal(employee);
-	      assertEquals(500, appraisal, 0.0);
-	   }
+    //test to check appraisal
+    @Test
+    public void testCalculateAppriasal() {
+        employee.setName("Rajeev");
+        employee.setAge(25);
+        employee.setMonthlySalary(8000);
 
-	   // test to check yearly salary
-	   @Test
-	   public void testCalculateYearlySalary() {
-	      employee.setName("Rajeev");
-	      employee.setAge(25);
-	      employee.setMonthlySalary(8000);
-			
-	      double salary = empBusinessLogic.calculateYearlySalary(employee);
-	      assertEquals(96000, salary, 0.0);
-	   }
+        double appraisal = empBusinessLogic.calculateAppraisal(employee);
+        Assert.assertEquals(500, appraisal, 0.0);
+    }
+
+    // test to check yearly salary
+    @Test
+    public void testCalculateYearlySalary() {
+        employee.setName("Rajeev");
+        employee.setAge(25);
+        employee.setMonthlySalary(8000);
+
+        double salary = empBusinessLogic.calculateYearlySalary(employee);
+        Assert.assertEquals(96000, salary, 0.0);
+    }
 
 }
