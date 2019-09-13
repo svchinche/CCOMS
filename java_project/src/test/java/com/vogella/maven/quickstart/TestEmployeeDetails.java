3010f2025fb8131a3a365a1f430638acfe9c8d5d
schinche <schinche@oracle.com>
2019-09-13 13:42:52 +0530

    Added integration_test changes

--- java_project/src/test/java/com/vogella/maven/quickstart/TestEmployeeDetails.java
+++ java_project/src/test/java/com/vogella/maven/quickstart/TestEmployeeDetails.java
@@ -10,29 +10,29 @@ import static org.hamcrest.Matchers.is;
 
 
 public class TestEmployeeDetails {
-	 EmpBusinessLogic empBusinessLogic = new EmpBusinessLogic();
-	   EmployeeDetails employee = new EmployeeDetails();
+    EmpBusinessLogic empBusinessLogic = new EmpBusinessLogic();
+    EmployeeDetails employee = new EmployeeDetails();
 
-	   //test to check appraisal
-	   @Test
-	   public void testCalculateAppriasal() {
-	      employee.setName("Rajeev");
-	      employee.setAge(25);
-	      employee.setMonthlySalary(8000);
-			
-	      double appraisal = empBusinessLogic.calculateAppraisal(employee);
-	      Assert.assertEquals(500, appraisal, 0.0);
-	   }
+    //test to check appraisal
+    @Test
+    public void testCalculateAppriasal() {
+        employee.setName("Rajeev");
+        employee.setAge(25);
+        employee.setMonthlySalary(8000);
 
-	   // test to check yearly salary
-	   @Test
-	   public void testCalculateYearlySalary() {
-	      employee.setName("Rajeev");
-	      employee.setAge(25);
-	      employee.setMonthlySalary(8000);
-			
-	      double salary = empBusinessLogic.calculateYearlySalary(employee);
-	      Assert.assertEquals(96000, salary, 0.0);
-	   }
+        double appraisal = empBusinessLogic.calculateAppraisal(employee);
+        Assert.assertEquals(500, appraisal, 0.0);
+    }
+
+    // test to check yearly salary
+    @Test
+    public void testCalculateYearlySalary() {
+        employee.setName("Rajeev");
+        employee.setAge(25);
+        employee.setMonthlySalary(8000);
+
+        double salary = empBusinessLogic.calculateYearlySalary(employee);
+        Assert.assertEquals(96000, salary, 0.0);
+    }
 
 }
