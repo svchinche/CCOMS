8f77a85bd86fa3c7c2525d6c5a21afe15badf21f
schinche <schinche@oracle.com>
2019-09-28 08:28:02 +0530

    Modified pom.xml file

--- springEureka/eureka-server/src/test/java/com/capitalessm/eurekanmingserver/NetflixEurekaNamingServerApplicationTests.java
+++ springEureka/eureka-server/src/test/java/com/capitalessm/eurekanmingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -1,5 +1,7 @@
 package com.capitalessm.eurekanmingserver;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class NetflixEurekaNamingServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	    assertTrue(true);
 	}
 
 }
