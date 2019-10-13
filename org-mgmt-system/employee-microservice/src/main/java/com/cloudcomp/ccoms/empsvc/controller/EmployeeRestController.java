1e711049376919790f0b1d87b8cf9f8cc6491670
schinche <schinche@oracle.com>
2019-10-13 19:12:33 +0530

    improved the ignore file

--- org-mgmt-system/employee-microservice/src/main/java/com/cloudcomp/ccoms/empsvc/controller/EmployeeRestController.java
+++ org-mgmt-system/employee-microservice/src/main/java/com/cloudcomp/ccoms/empsvc/controller/EmployeeRestController.java
@@ -1,16 +1,15 @@
 package com.cloudcomp.ccoms.empsvc.controller;
-import java.math.BigInteger;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import com.cloudcomp.ccoms.empsvc.model.Employee;
 import com.cloudcomp.ccoms.empsvc.repository.EmployeeRepository;
 
@@ -109,7 +107,7 @@ public class EmployeeRestController {
         return new ResponseEntity<Employee>(usr, HttpStatus.OK);
     }
 
-
+    
     @GetMapping("/dept/{deptId}")
     public List<Employee> findByDepartment(@PathVariable("deptId") int deptId) {
         LOGGER.info("Employee find: departmentId={}", deptId);
