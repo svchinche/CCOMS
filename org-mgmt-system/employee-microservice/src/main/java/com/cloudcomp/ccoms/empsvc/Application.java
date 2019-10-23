54d61d502a207b53898f3ad79041da3cecd736ca
schinche <schinche@oracle.com>
2019-10-23 20:09:11 +0530

    Added wait logic till application get the conf from conf server

--- org-mgmt-system/employee-microservice/src/main/java/com/cloudcomp/ccoms/empsvc/Application.java
+++ org-mgmt-system/employee-microservice/src/main/java/com/cloudcomp/ccoms/empsvc/Application.java
@@ -7,23 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-import io.swagger.models.Contact;
-import springfox.documentation.PathProvider;
+import org.springframework.retry.annotation.EnableRetry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
+
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
+@EnableRetry
 @EnableSwagger2
+@Configuration
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application {
     public static void main(String[] args) {
--- org-mgmt-system/organization-microservice/src/main/java/com/cloudcomp/ccoms/orgsvc/Application.java
+++ org-mgmt-system/organization-microservice/src/main/java/com/cloudcomp/ccoms/orgsvc/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -14,6 +15,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableRetry
 @EnableFeignClients
 @EnableSwagger2
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
