73728112798887e9d86c91592e1bf6aa53915404
suyog chinche <suyog.v.chiche@oracle.com>
2019-11-22 21:35:48 -0500

    made some changes in curlfile for sending data

--- org-mgmt-system/kubernetes/ansible_k8s-ccoms-deployment/scripts/post_ccoms/curl_senddatausingpostmethod.sh
+++ org-mgmt-system/kubernetes/ansible_k8s-ccoms-deployment/scripts/post_ccoms/curl_senddatausingpostmethod.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 export NO_PROXY=mum00aqm,localhost,127.0.0.1
 HOST_NAME=10.180.86.187
 PROXY_PORT=8111
-EMP_JSONFILE="../../../org_management_system/employee-microservice/emps.json"
-DEPT_JSONFILE="../../../org_management_system/department-microservice/departments.json"
-ORG_JSONFILE="../../../org_management_system/organization-microservice/organization.json"
+
+JSON_FILES_DIR="$SCRIPTPATH/json_files"
+EMP_JSONFILE="$JSON_FILES_DIR/emps.json"
+DEPT_JSONFILE="$JSON_FILES_DIR/departments.json"
+ORG_JSONFILE="$JSON_FILES_DIR/organization.json"
 
 
 curl -X POST -H "Content-Type: application/json" -d @$EMP_JSONFILE http://$HOST_NAME:$PROXY_PORT/emp/api/addemps
