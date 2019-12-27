64388c651d61467edc6fff03ac88e4152b0fe386
schinche <schinche@oracle.com>
2019-12-27 07:13:05 +0530

    Removed java code coverage verification on configservice and gateway service since they do not contain controllers and they are using for configuration purpose

--- organization-management-system/config-service/src/test/java/com/cloudcomp/ccoms/configserver/ConfigServerMicroserviceApplicationTests.java
+++ organization-management-system/config-service/src/test/java/com/cloudcomp/ccoms/configserver/ConfigServerMicroserviceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.cloudcomp.ccoms.configserver;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@ExtendWith(MockitoExtension.class)
 public class ConfigServerMicroserviceApplicationTests {
 
 	@Test
