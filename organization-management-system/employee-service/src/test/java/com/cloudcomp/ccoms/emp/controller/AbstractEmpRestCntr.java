0e65097822ad7fcf729fbfa035bb8163cd24071a
schinche <schinche@oracle.com>
2020-01-12 12:28:06 +0530

    Cucumber BDD support on employee service

---
+++ organization-management-system/employee-service/src/test/java/com/cloudcomp/ccoms/emp/controller/AbstractEmpRestCntr.java
@@ -0,0 +1,25 @@
+package com.cloudcomp.ccoms.emp.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.cloudcomp.ccoms.emp.Application;
+import com.cloudcomp.ccoms.emp.dao.EmployeeRepository;
+
+
+
+@ContextConfiguration
+@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class AbstractEmpRestCntr {
+    
+    @MockBean
+    private EmployeeRepository emprepo;
+    
+}
--- organization-management-system/employee-service/src/test/java/com/cloudcomp/ccoms/emp/controller/EmpRestStepDefs.java
+++ organization-management-system/employee-service/src/test/java/com/cloudcomp/ccoms/emp/controller/EmpRestStepDefs.java
@@ -1,5 +1,12 @@
 package com.cloudcomp.ccoms.emp.controller;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java8.En;
+import io.cucumber.datatable.DataTable;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -8,22 +15,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.ui.Model;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import com.cloudcomp.ccoms.emp.Application;
 import com.cloudcomp.ccoms.emp.dao.EmployeeRepository;
 import com.cloudcomp.ccoms.emp.model.Employee;
 import com.openpojo.reflection.PojoClass;
@@ -34,17 +35,11 @@ import com.openpojo.validation.rule.impl.GetterMustExistRule;
 import com.openpojo.validation.rule.impl.SetterMustExistRule;
 import com.openpojo.validation.test.impl.GetterTester;
 import com.openpojo.validation.test.impl.SetterTester;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-@ExtendWith(MockitoExtension.class)
+public class EmpRestStepDefs extends AbstractEmpRestCntr implements En {
 
-public class EmployeeRestControllerTest {
-
-    List<Employee> emps;
-    private final Logger log = LoggerFactory.getLogger(EmployeeRestControllerTest.class);
+    List<Employee> emps = new ArrayList<Employee>();
+    private final Logger log = LoggerFactory.getLogger(EmpRestStepDefs.class);
 
     final Employee emp1 = new Employee(1L, "Suyog", 29, "DevOps Engineer", 21, 2);
     final Employee emp2 = new Employee(5L, "Sanjay", 29, "DevOps Engineer", 21, 2);
@@ -53,7 +48,7 @@ public class EmployeeRestControllerTest {
     @Autowired
     private EmployeeRestController empRestController;
 
-    @MockBean
+    @Autowired
     private EmployeeRepository emprepo;
 
     @InjectMocks
@@ -63,19 +58,23 @@ public class EmployeeRestControllerTest {
     @Mock
     private Model model;
 
-    private List<Employee> given_emp_dataset() {
-        emps = new ArrayList<Employee>();
-        Employee emp_a = new Employee(1L, "Suyog", 29, "DevOps Engineer", 21, 2);
-        Employee emp_b = new Employee(2L, "Sachin", 29, "DevOps Engineer-2", 11, 1);
-        Employee emp_c = new Employee(3L, "Suraj", 29, "DevOps Engineer-2", 13, 1);
-        emps.add(emp_a);
-        emps.add(emp_b);
-        emps.add(emp_c);
-        return emps;
+    @Before
+    public void setUp() {
+        log.info("-------------- Spring Context Initialized For Executing Cucumber Tests --------------");
     }
 
+    @After
+    public void end() {
+        log.info("-------------- End scenario --------------");
+    }
+
+    @Given("Following Employee information is available in store")
+    public void given_emp_dataset(DataTable empDataTable) {
+        this.emps = empDataTable.asList(Employee.class);
+    }
+
+    @When("I set HTTP GET request RESTful API with URI {long}")
     public void get_rest_api_to_get_single_emp_info(final Long empid) {
-        emps = given_emp_dataset();
         if (empid >= 3L) {
             when(emprepo.findById(empid)).thenReturn(Optional.empty());
         } else {
@@ -83,13 +82,23 @@ public class EmployeeRestControllerTest {
         }
     }
 
+    @When("I set HTTP GET request RESTful API with get URI")
     public void get_rest_api_to_get_emps_info() {
-        emps = given_emp_dataset();
         when(emprepo.findAll()).thenReturn(emps);
     }
 
+    @When("I set HTTP GET request RESTful API with pretty URI")
+    public void i_set_HTTP_GET_request_RESTful_API_with_pretty_URI() {
+        when(emprepo.findAll()).thenReturn(emps);
+    }
+
+    @When("I set HTTP DELETE REST API with URI {int}")
+    public void i_set_HTTP_DELETE_REST_API_with_URI(Integer int1) {
+
+    }
+
+    @When("I set HTTP GET request RESTful API with dept with {int} id URI")
     public void get_rest_api_to_get_emp_info_using_deptid(final int dept_id) {
-        emps = given_emp_dataset();
         ArrayList<Employee> emp_with_depts = new ArrayList<Employee>();
         for (Employee emp : emps) {
             if (emp.getDeptId() == dept_id) {
@@ -99,8 +108,8 @@ public class EmployeeRestControllerTest {
         when(emprepo.findEmpsByDeptId(dept_id)).thenReturn(emp_with_depts);
     }
 
+    @When("I set HTTP GET request RESTful API with org with {int} id URI")
     public void get_rest_api_to_get_emps_using_orgid(final int org_id) {
-        emps = given_emp_dataset();
         ArrayList<Employee> emp_with_orgs = new ArrayList<Employee>();
         for (Employee emp : emps) {
             if (emp.getOrgId() == org_id) {
@@ -110,8 +119,14 @@ public class EmployeeRestControllerTest {
         when(emprepo.findEmpsByOrgId(org_id)).thenReturn(emp_with_orgs);
     }
 
-    public void post_rest_api_to_save_empl_using_id(final Employee emp) {
-        emps = given_emp_dataset();
+    @When("I set HTTP GET request without any resource mentioned in API")
+    public String set_rest_api_without_resource() {
+        return empRestController.get();
+    }
+
+    @When("I set HTTP POST request RESTful API with resource URI")
+    public void post_rest_api_to_save_empl_using_id() {
+        Employee emp = emp1;
         if (emp.getId() >= 3L) {
             when(emprepo.findById(emp.getId())).thenReturn(Optional.empty());
         } else {
@@ -120,102 +135,82 @@ public class EmployeeRestControllerTest {
         }
     }
 
+    @When("I set HTTP POST request RESTful API with resource URI - all at once")
     public void post_rest_api_to_save_emps() {
         when(emprepo.saveAll(emps)).thenReturn(emps);
     }
 
-    @Test
+    @Then("I should get valid HTTP response code 200 of GET request for respective employee information")
     public void testGetEmpById() throws ResourceNotFoundException {
-
-        // Given
-        given_emp_dataset();
-
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-        // when
-        get_rest_api_to_get_single_emp_info(1L);
-        get_rest_api_to_get_single_emp_info(4L);
         ResponseEntity<Employee> responseEntity = empRestController.getEmpById(1L);
-        ResponseEntity<Employee> negResponseEntity = empRestController.getEmpById(4L);
-
-        // then
         assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
-        assertThat(negResponseEntity.getStatusCodeValue()).isEqualTo(404);
     }
 
-    @Test
+    @Then("I should get valid HTTP response code 200 of POST request to add one employee at a time")
     public void testAddEmp() {
-
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-        // when
-        post_rest_api_to_save_empl_using_id(emp1);
         ResponseEntity<Object> responseEntity = empRestController.addEmp(emp1);
-
-        // then
         assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
         assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/api/1");
     }
 
-    @Test
+    @Then("I should get valid HTTP response code 200 of POST request to add all employee at once")
     public void testAddEmps() {
-
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-        // when
-        post_rest_api_to_save_emps();
         ResponseEntity<Object> responseEntity = empRestController.addEmps(emps);
-
-        // then
         assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
         assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/api/all");
     }
 
-    @Test
+    @Then("I should get valid HTTP response code 200 of GET request for all emps")
     public void testGetAllEmps() {
-
-        // Given -- Setup eg. initializing or preparing
-        get_rest_api_to_get_emps_info();
-        // when
         int empcount = empRestController.getAllEmps().size();
-        // then
         Assertions.assertEquals(3, empcount);
     }
 
-    @Test
-    public void testUpdateEmp() throws ResourceNotFoundException {
+    @Then("I should get valid HTTP response code 200 of GET request for respective dept")
+    public void testFindByDept() {
+        int dept_id = 11;
+        List<Employee> emps = empRestController.findByDept(dept_id);
+        Assertions.assertEquals(1, emps.size());
+    }
 
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-        // Given -- Setup eg. initializing or preparing
+    @Then("I should get valid HTTP response code 200 of GET request for respective org")
+    public void testFindByOrg() {
+        int org_id = 2;
+        List<Employee> emps = empRestController.findByOrg(org_id);
+        Assertions.assertEquals(1, emps.size());
+    }
 
-        post_rest_api_to_save_empl_using_id(emp1);
-        post_rest_api_to_save_empl_using_id(emp2);
+    @Then("I should get valid HTTP response code 200 for that request with warning message")
+    public void testGet() {
+        String actual = "Please give url as hostname/employee/get";
+        Assertions.assertEquals(actual, set_rest_api_without_resource());
+    }
 
-        // when
-        ResponseEntity<Object> negResponseEntity = empRestController.updateEmp(emp2);
-        ResponseEntity<Object> responseEntity = empRestController.updateEmp(emp1);
-
-        // then
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
-        assertThat(negResponseEntity.getStatusCodeValue()).isEqualTo(404);
+    @Then("I should get valid HTTP response code 200 of GET request with all employees information in UI")
+    void testShowPrettyOutput() {
+        int empcount = empRestController.getAllEmps().size();
+        String return_val = empViewController.showPrettyOutput(model);
+        Assertions.assertEquals(3, empcount);
+        Assertions.assertEquals("show_pretty_output", return_val);
 
     }
 
-    @Test
-    public void testDeleteEmp() throws ResourceNotFoundException {
+    @Then("I should get valid HTTP response code 200 of GET request with all employees information in UI")
+    public void i_should_get_valid_HTTP_response_code_of_GET_request_with_all_employees_information_in_UI() {
+        // Write code here that turns the phrase above into concrete actions
+        int empcount = empRestController.getAllEmps().size();
+        Assertions.assertEquals(3, empcount);
+    }
 
-        // Given
-
-        Employee emp1 = new Employee(1L, "Suyog", 29, "DevOps Engineer", 21, 2);
-        Employee emp2 = new Employee(5L, "Sanjay", 29, "DevOps Engineer", 21, 2);
-        post_rest_api_to_save_empl_using_id(emp1);
-        post_rest_api_to_save_empl_using_id(emp2);
-
-        // when
+    @Then("I should get valid HTTP response code 200 of DELETE request to delete Employee")
+    public void i_should_get_valid_HTTP_response_code_of_DELETE_request_to_delete_Employee() {
+        // Write code here that turns the phrase above into concrete actions
         empRestController.deleteEmp(1L);
         ResponseEntity<Object> negResponseEntity = empRestController.deleteEmp(5L);
 
@@ -224,73 +219,13 @@ public class EmployeeRestControllerTest {
         assertThat(negResponseEntity.getStatusCodeValue()).isEqualTo(404);
     }
 
-    @Test
-    public void testUpdateEmpPartially() throws ResourceNotFoundException {
-
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-        post_rest_api_to_save_empl_using_id(emp1);
-        post_rest_api_to_save_empl_using_id(emp2);
-
-        // when
-        ResponseEntity<Object> responseEntity = empRestController.updateEmpPartially(1L, emp1);
-        ResponseEntity<Object> negResponseEntity = empRestController.updateEmpPartially(5L, emp2);
-
-        // then
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
-        assertThat(negResponseEntity.getStatusCodeValue()).isEqualTo(404);
+    @When("I initialize all Getters and Setters")
+    public void setGetterSetter() {
+        log.info("----------Initializing Getters and Settters----------");
     }
 
-    @Test
-    public void testFindByDept() {
-        int dept_id = 11;
-
-        get_rest_api_to_get_emp_info_using_deptid(dept_id);
-        // when
-        List<Employee> emps = empRestController.findByDept(dept_id);
-
-        // then
-        Assertions.assertEquals(1, emps.size());
-    }
-
-    @Test
-    public void testFindByOrg() {
-        int org_id = 2;
-
-        // Given -- Setup eg. initializing or preparing
-        get_rest_api_to_get_emps_using_orgid(org_id);
-        // when
-        List<Employee> emps = empRestController.findByOrg(org_id);
-
-        // then
-        Assertions.assertEquals(1, emps.size());
-    }
-
-    @Test
-    public void testGet() {
-        String actual = empRestController.get();
-        Assertions.assertEquals("Please give url as hostname/employee/get", actual);
-    }
-
-    @Test
-    void testShowPrettyOutput() {
-
-        // Given -- Setup eg. initializing or preparing
-        get_rest_api_to_get_emps_info();
-        // when
-        int empcount = empRestController.getAllEmps().size();
-
-        String return_val = empViewController.showPrettyOutput(model);
-
-        // then
-        Assertions.assertEquals(3, empcount);
-        Assertions.assertEquals("show_pretty_output", return_val);
-
-    }
-
-    @Test
-    public void testGetterSetter() {
+    @Then("All Getters and Setters should be initialized")
+    public void verifyGetterSetter() {
         PojoClass pojoclass = PojoClassFactory.getPojoClass(Employee.class);
         Validator validator = ValidatorBuilder.create().with(new SetterMustExistRule()).with(new GetterMustExistRule())
                 .with(new SetterTester()).with(new GetterTester()).build();
