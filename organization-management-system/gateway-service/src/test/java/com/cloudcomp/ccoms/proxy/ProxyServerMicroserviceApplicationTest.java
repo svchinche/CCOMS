b536d1799bd5f760c63ea3e7813b57b82984af87
schinche <schinche@oracle.com>
2020-01-13 09:26:00 +0530

    Updated random port for proxy service

--- organization-management-system/gateway-service/src/test/java/com/cloudcomp/ccoms/proxy/ProxyServerMicroserviceApplicationTest.java
+++ organization-management-system/gateway-service/src/test/java/com/cloudcomp/ccoms/proxy/ProxyServerMicroserviceApplicationTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = ProxyServerMicroserviceApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 public class ProxyServerMicroserviceApplicationTest {
